ab6a890c21aa8405bac607f2cf8e16ea5f473d4b
Mohamed Ramadan <mhmdrmdan652@gmail.com>
2025-04-27 17:46:04 +0300

    first question

---
+++ script/External.js
@@ -0,0 +1,13 @@
+let nuber1 = 3,
+    number2 = 2.9,
+    number3 = 0xff;
+
+var firstName = "Mohamed",
+    middleName = 'Ramadan',
+    lastName = `Abdulla`;
+
+let flagVariable = true;
+
+console.log("This is the External JavaScript file");
+
+
