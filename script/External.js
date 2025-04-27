0cf3d81842d7387426e39c13f03d1603e7608316
Mohamed Ramadan <mhmdrmdan652@gmail.com>
2025-04-27 17:59:54 +0300

    Question number 3

--- script/External.js
+++ script/External.js
@@ -1,13 +1,20 @@
-let nuber1 = 3,
-    number2 = 2.9,
-    number3 = 0xff;
+// let nuber1 = 3,
+//     number2 = 2.9,
+//     number3 = 0xff;
 
-var firstName = "Mohamed",
-    middleName = 'Ramadan',
-    lastName = `Abdulla`;
+// var firstName = "Mohamed",
+//     middleName = 'Ramadan',
+//     lastName = `Abdulla`;
 
-let flagVariable = true;
+// let flagVariable = true;
 
-console.log("This is the External JavaScript file");
+// console.log("This is the External JavaScript file");
+
+// console.log(number1);
+// let number1 = 3;
+// External.js: 13 Uncaught ReferenceError: Cannot access 'number1' before initialization
+//     at External.js: 13: 13
+// it will not print number1 because number1 was not defined yet when it run the console.log
+
 
 
